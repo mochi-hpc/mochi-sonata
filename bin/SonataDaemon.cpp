2612344d4a553349e8b3e168f3d0a6bbc463fd7f
Matthieu Dorier <mdorier@anl.gov>
2021-07-26 10:39:49 +0000

    added possibility to pass a config file to sonata daemon

--- bin/SonataDaemon.cpp
+++ bin/SonataDaemon.cpp
@@ -8,6 +8,8 @@
 #include <spdlog/spdlog.h>
 #include <tclap/CmdLine.h>
 #include <vector>
+#include <fstream>
+#include <streambuf>
 
 namespace tl = thallium;
 namespace snt = sonata;
@@ -17,14 +19,23 @@ static unsigned g_num_providers = 1;
 static int g_num_threads = 0;
 static std::string g_log_level = "info";
 static bool g_use_progress_thread = false;
+static std::string g_config = "";
 
 static void parse_command_line(int argc, char **argv);
 
 int main(int argc, char **argv) {
   parse_command_line(argc, argv);
-  spdlog::set_level(spdlog::level::from_str(g_log_level));
-  tl::engine engine(g_address, THALLIUM_SERVER_MODE, g_use_progress_thread,
-                    g_num_threads);
+  tl::engine engine;
+  if(g_config.empty()) {
+    engine = tl::engine(g_address, THALLIUM_SERVER_MODE,
+                        g_use_progress_thread, g_num_threads);
+  } else {
+    std::ifstream f(g_config.c_str());
+    std::string config((std::istreambuf_iterator<char>(f)),
+                        std::istreambuf_iterator<char>());
+    engine = tl::engine(g_address, THALLIUM_SERVER_MODE,
+                        config.c_str());
+  }
   engine.enable_remote_shutdown();
   std::vector<snt::Provider> providers;
   for (unsigned i = 0; i < g_num_providers; i++) {
@@ -50,6 +61,9 @@ void parse_command_line(int argc, char **argv) {
     TCLAP::ValueArg<int> numThreads("t", "num-threads",
                                     "Number of threads for RPC handlers", false,
                                     0, "int");
+    TCLAP::ValueArg<std::string> configArg("c", "config",
+                                           "Thallium engine JSON configuration file", false,
+                                           "", "string");
     TCLAP::ValueArg<std::string> logLevel(
         "v", "verbose",
         "Log level (trace, debug, info, warning, error, critical, off)", false,
@@ -58,12 +72,19 @@ void parse_command_line(int argc, char **argv) {
     cmd.add(providersArg);
     cmd.add(numThreads);
     cmd.add(logLevel);
+    cmd.add(configArg);
     cmd.parse(argc, argv);
+    g_log_level = logLevel.getValue();
+    spdlog::set_level(spdlog::level::from_str(g_log_level));
     g_address = addressArg.getValue();
     g_num_providers = providersArg.getValue();
     g_num_threads = numThreads.getValue();
     g_use_progress_thread = progressThreadArg.getValue();
-    g_log_level = logLevel.getValue();
+    g_config = configArg.getValue();
+    if(configArg.isSet() && numThreads.isSet())
+        spdlog::warn("--config and --num-threads both set, --num-threads will be ignored");
+    if(configArg.isSet() && progressThreadArg.isSet())
+        spdlog::warn("--config and --use-progress-thread both set, --use-progress-thread will be ignored");
   } catch (TCLAP::ArgException &e) {
     std::cerr << "error: " << e.error() << " for arg " << e.argId()
               << std::endl;
