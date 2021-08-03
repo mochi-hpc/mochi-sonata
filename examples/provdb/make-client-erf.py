58759c3244974b8d0c457f7bc4b818a894e7f883
Matthieu Dorier <mdorier@anl.gov>
2021-08-03 17:04:51 -0400

    fixed erf generation

--- examples/provdb/make-client-erf.py
+++ examples/provdb/make-client-erf.py
@@ -3,9 +3,9 @@ import sys
 def make_client_erf(num_server_nodes, num_nodes, clients_per_node):
     print('cpu_index_using: logical')
     num_ranks = num_nodes*clients_per_node
-    cpu_per_rank = 168/clients_per_node
+    cpu_per_rank = 168 // clients_per_node
     for rank in range(0, num_ranks):
-        host = rank / clients_per_node
+        host = rank // clients_per_node
         cpu_min = (rank - host*clients_per_node) * cpu_per_rank
         cpu_max = cpu_min + cpu_per_rank - 1
         print('rank: %d: { host: %d; cpu: { %d-%d } } : app 0' % (rank, (host+num_server_nodes)+1, cpu_min, cpu_max))
--- examples/provdb/make-server-erf.py
+++ examples/provdb/make-server-erf.py
@@ -3,9 +3,9 @@ import sys
 def make_server_erf(num_nodes, servers_per_node):
     print('cpu_index_using: logical')
     num_ranks = num_nodes*servers_per_node
-    cpu_per_rank = 168/servers_per_node
+    cpu_per_rank = 168 // servers_per_node
     for rank in range(0, num_ranks):
-        host = rank / servers_per_node
+        host = rank // servers_per_node
         cpu_min = (rank - host*servers_per_node) * cpu_per_rank
         cpu_max = cpu_min + cpu_per_rank - 1
         print('rank: %d: { host: %d; cpu: { %d-%d } } : app 0' % (rank, host+1, cpu_min, cpu_max))
